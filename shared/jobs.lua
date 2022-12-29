8468974e849b1d96253386b9e90281fdc07c705f
RexShack <rexshackuk@gmail.com>
2022-12-29 15:17:32 +0000

    Update jobs.lua

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -46,8 +46,248 @@ RSGShared.Jobs = {
             },
         },
     },
-    ['saloontender'] = {
-        label = 'Saloon Tender',
+    ['valsaloontender'] = { --valentine
+        label = 'Valentine Saloon',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Trainee',
+                payment = 25
+            },
+            ['1'] = {
+                name = 'Tender',
+                payment = 50
+            },
+            ['2'] = {
+                name = 'Manager',
+                isboss = true,
+                payment = 75
+            },
+        },
+    },
+    ['blasaloontender'] = { --blackwater
+        label = 'Blackwater Saloon',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Trainee',
+                payment = 25
+            },
+            ['1'] = {
+                name = 'Tender',
+                payment = 50
+            },
+            ['2'] = {
+                name = 'Manager',
+                isboss = true,
+                payment = 75
+            },
+        },
+    },
+    ['rhosaloontender'] = { --rhodes
+        label = 'Rhodes Saloon',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Trainee',
+                payment = 25
+            },
+            ['1'] = {
+                name = 'Tender',
+                payment = 50
+            },
+            ['2'] = {
+                name = 'Manager',
+                isboss = true,
+                payment = 75
+            },
+        },
+    },
+    ['stdenissaloontender1'] = { --saint denis 1
+        label = 'Saint Denis Saloon 1',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Trainee',
+                payment = 25
+            },
+            ['1'] = {
+                name = 'Tender',
+                payment = 50
+            },
+            ['2'] = {
+                name = 'Manager',
+                isboss = true,
+                payment = 75
+            },
+        },
+    },
+    ['stdenissaloontender2'] = { --saint denis 2
+        label = 'Saint Denis Saloon 2',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Trainee',
+                payment = 25
+            },
+            ['1'] = {
+                name = 'Tender',
+                payment = 50
+            },
+            ['2'] = {
+                name = 'Manager',
+                isboss = true,
+                payment = 75
+            },
+        },
+    },
+    ['vansaloontender'] = { --van horn
+        label = 'Van Horn Saloon',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Trainee',
+                payment = 25
+            },
+            ['1'] = {
+                name = 'Tender',
+                payment = 50
+            },
+            ['2'] = {
+                name = 'Manager',
+                isboss = true,
+                payment = 75
+            },
+        },
+    },
+    ['armsaloontender'] = { --armadillo
+        label = 'Armadillo Saloon',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Trainee',
+                payment = 25
+            },
+            ['1'] = {
+                name = 'Tender',
+                payment = 50
+            },
+            ['2'] = {
+                name = 'Manager',
+                isboss = true,
+                payment = 75
+            },
+        },
+    },
+    ['tumsaloontender'] = { --tumbleweed
+        label = 'Tumbleweed Saloon',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Trainee',
+                payment = 25
+            },
+            ['1'] = {
+                name = 'Tender',
+                payment = 50
+            },
+            ['2'] = {
+                name = 'Manager',
+                isboss = true,
+                payment = 75
+            },
+        },
+    },
+    ['moonsaloontender1'] = { --lemoyne speakeasy
+        label = 'Lemoyne Speakeasy',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Trainee',
+                payment = 25
+            },
+            ['1'] = {
+                name = 'Tender',
+                payment = 50
+            },
+            ['2'] = {
+                name = 'Manager',
+                isboss = true,
+                payment = 75
+            },
+        },
+    },
+    ['moonsaloontender2'] = { --new austin speakeasy
+        label = 'New Austin Speakeasy',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Trainee',
+                payment = 25
+            },
+            ['1'] = {
+                name = 'Tender',
+                payment = 50
+            },
+            ['2'] = {
+                name = 'Manager',
+                isboss = true,
+                payment = 75
+            },
+        },
+    },
+    ['moonsaloontender3'] = { --cattail pond speakeasy
+        label = 'Cattail Pond Speakeasy',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Trainee',
+                payment = 25
+            },
+            ['1'] = {
+                name = 'Tender',
+                payment = 50
+            },
+            ['2'] = {
+                name = 'Manager',
+                isboss = true,
+                payment = 75
+            },
+        },
+    },
+    ['moonsaloontender4'] = { --hanover speakeasy
+        label = 'Hanover Speakeasy',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Trainee',
+                payment = 25
+            },
+            ['1'] = {
+                name = 'Tender',
+                payment = 50
+            },
+            ['2'] = {
+                name = 'Manager',
+                isboss = true,
+                payment = 75
+            },
+        },
+    },
+    ['moonsaloontender5'] = { --manzanita post speakeasy
+        label = 'Manzanita Post Speakeasy',
         defaultDuty = true,
         offDutyPay = false,
         grades = {
