a16c8b165d81bc5f9340cdac6f88f341203b8e80
RexshackGaming <rexshackuk@gmail.com>
2023-09-21 15:55:00 +0100

    add blacksmith job

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -68,4 +68,13 @@ RSGShared.Jobs = {
             ['1'] = { name = 'Master',  isboss = true, payment = 75 },
         },
     },
+    ['valblacksmith'] = { --valentine
+        label = 'Valentine Blacksmith',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = { name = 'Trainee', payment = 25 },
+            ['1'] = { name = 'Master', isboss = true, payment = 75 },
+        },
+    },
 }
