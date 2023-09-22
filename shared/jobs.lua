271aab1f78b60a9cd5d0f3d172dfa7df653869fc
RexshackGaming <rexshackuk@gmail.com>
2023-09-22 12:06:28 +0100

    Update jobs.lua

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -11,6 +11,18 @@ RSGShared.Jobs = {
             ['0'] = { name = 'Freelancer', payment = 5 },
         },
     },
+    ['police'] = {
+        label = 'Law Enforcement',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = { name = 'Recruit', payment = 50 },
+            ['1'] = { name = 'Officer', payment = 75 },
+            ['2'] = { name = 'Sergeant', payment = 100 },
+            ['3'] = { name = 'Lieutenant', payment = 125 },
+            ['4'] = { name = 'Chief', isboss = true, payment = 150 },
+        },
+    },
     ['medic'] = {
         label = 'Medic',
         defaultDuty = true,
