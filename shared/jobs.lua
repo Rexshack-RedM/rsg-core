46b78f169636c38929fbd73194e82bf2abd46aba
RexshackGaming <rexshackuk@gmail.com>
2023-09-23 11:50:59 +0100

    Update jobs.lua

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -107,4 +107,85 @@ RSGShared.Jobs = {
             ['1'] = { name = 'Farm Owner', isboss = true, payment = 75 },
         },
     },
+    ['valsaloontender'] = { --valentine
+        label = 'Valentine Saloon',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = { name = 'Trainee', payment = 25 },
+            ['1'] = { name = 'Tender',  payment = 50 },
+            ['2'] = { name = 'Manager', isboss = true, payment = 75 },
+        },
+    },
+    ['blasaloontender'] = { --blackwater
+        label = 'Blackwater Saloon',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = { name = 'Trainee', payment = 25 },
+            ['1'] = { name = 'Tender',  payment = 50 },
+            ['2'] = { name = 'Manager', isboss = true, payment = 75 },
+        },
+    },
+    ['rhosaloontender'] = { --rhodes
+        label = 'Rhodes Saloon',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = { name = 'Trainee', payment = 25 },
+            ['1'] = { name = 'Tender',  payment = 50 },
+            ['2'] = { name = 'Manager', isboss = true, payment = 75 },
+        },
+    },
+    ['stdenissaloontender1'] = { --saint denis 1
+        label = 'Saint Denis Saloon 1',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = { name = 'Trainee', payment = 25 },
+            ['1'] = { name = 'Tender',  payment = 50 },
+            ['2'] = { name = 'Manager', isboss = true, payment = 75 },
+        },
+    },
+    ['stdenissaloontender2'] = { --saint denis 2
+        label = 'Saint Denis Saloon 2',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = { name = 'Trainee', payment = 25 },
+            ['1'] = { name = 'Tender',  payment = 50 },
+            ['2'] = { name = 'Manager', isboss = true, payment = 75 },
+        },
+    },
+    ['vansaloontender'] = { --van horn
+        label = 'Van Horn Saloon',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = { name = 'Trainee', payment = 25 },
+            ['1'] = { name = 'Tender',  payment = 50 },
+            ['2'] = { name = 'Manager', isboss = true, payment = 75 },
+        },
+    },
+    ['armsaloontender'] = { --armadillo
+        label = 'Armadillo Saloon',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = { name = 'Trainee', payment = 25 },
+            ['1'] = { name = 'Tender',  payment = 50 },
+            ['2'] = { name = 'Manager', isboss = true, payment = 75 },
+        },
+    },
+    ['tumsaloontender'] = { --tumbleweed
+        label = 'Tumbleweed Saloon',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = { name = 'Trainee', payment = 25 },
+            ['1'] = { name = 'Tender',  payment = 50 },
+            ['2'] = { name = 'Manager', isboss = true, payment = 75 },
+        },
+    },
+	
 }
