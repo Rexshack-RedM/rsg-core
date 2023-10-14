3abf463d448b266aa8ffabb4bf51ad888eb3f46f
RexshackGaming <rexshackuk@gmail.com>
2023-10-14 19:16:48 +0100

    ranch jobs support

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -301,5 +301,27 @@ RSGShared.Jobs = {
             ['1'] = { name = 'Master',  isboss = true, payment = 75 },
         },
     },
+    ['macfarranch'] = {
+        label = 'Macfarlane Rancher',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = { name = 'Ranch Trainee', payment = 25 },
+            ['1'] = { name = 'Ranch Hand', payment = 30 },
+            ['2'] = { name = 'Ranch Manager', payment = 35 },
+            ['3'] = { name = 'Ranch Boss', isboss = true, payment = 40 },
+        },
+    },
+    ['prongranch'] = {
+        label = 'Pronghorn Ranch',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = { name = 'Ranch Trainee', payment = 25 },
+            ['1'] = { name = 'Ranch Hand', payment = 30 },
+            ['2'] = { name = 'Ranch Manager', payment = 35 },
+            ['3'] = { name = 'Ranch Boss', isboss = true, payment = 40 },
+        },
+    },
 
 }
