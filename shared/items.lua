1fb9e2e58d647697e6e9b9a6b7f5dd993485861f
RexshackGaming <rexshackuk@gmail.com>
2023-10-14 19:34:25 +0100

    ranch supporting items

--- shared/items.lua
+++ shared/items.lua
@@ -45,6 +45,15 @@ RSGShared.Items = {
     ['horsereviver']        = {['name'] = 'horsereviver',        ['label'] = 'Horse Reviver',  ['weight'] = 100, ['type'] = 'item', ['image'] = 'consumable_horse_reviver.png', ['unique'] = false, ['useable'] = true,  ['shouldClose'] = true,  ['combinable'] = nil, ['level'] = 0, ['description'] = 'used to revive a horse'},
     ['saddlebag']           = {['name'] = 'saddlebag',           ['label'] = 'Saddlebag',      ['weight'] = 650, ['type'] = 'item', ['image'] = 'saddlebag.png',                ['unique'] = true,  ['useable'] = true,  ['shouldClose'] = false, ['combinable'] = nil, ['level'] = 0, ['description'] = ''},
 
+    -- ranch items
+    ['animalfeed'] = {['name'] = 'animalfeed', ['label'] = 'Animal Feed', ['weight'] = 100, ['type'] = 'item', ['image'] = 'animalfeed.png', ['unique'] = false, ['useable'] = true,  ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ['description'] = 'ranch'},
+    ['milk']       = {['name'] = 'milk',       ['label'] = 'Milk',        ['weight'] = 100, ['type'] = 'item', ['image'] = 'milk.png',       ['unique'] = false, ['useable'] = true,  ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ['description'] = 'ranch'},
+    ['wool']       = {['name'] = 'wool',       ['label'] = 'Wool',        ['weight'] = 100, ['type'] = 'item', ['image'] = 'wool.png',       ['unique'] = false, ['useable'] = true,  ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ['description'] = 'ranch'},
+    ['egg']        = {['name'] = 'egg',        ['label'] = 'Egg',         ['weight'] = 100, ['type'] = 'item', ['image'] = 'egg.png',        ['unique'] = false, ['useable'] = true,  ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ['description'] = 'ranch'},
+    ['truffle']    = {['name'] = 'truffle',    ['label'] = 'Truffle',     ['weight'] = 100, ['type'] = 'item', ['image'] = 'truffle.png',    ['unique'] = false, ['useable'] = true,  ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ['description'] = 'ranch'},
+    ['chicken']    = {['name'] = 'chicken',    ['label'] = 'Chicken',     ['weight'] = 100, ['type'] = 'item', ['image'] = 'chicken.png',    ['unique'] = false, ['useable'] = true,  ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ['description'] = 'ranch'},
+    ['pig']        = {['name'] = 'pig',        ['label'] = 'Truffle Pig', ['weight'] = 100, ['type'] = 'item', ['image'] = 'pig.png',        ['unique'] = false, ['useable'] = true,  ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ['description'] = 'ranch'},
+
     -- farming items
     ['cornseed']     = {['name'] = 'cornseed',     ['label'] = 'Corn Seed',     ['weight'] = 0,   ['type'] = 'item', ['image'] = 'seeds.png',       ['unique'] = false, ['useable'] = true, ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ['description'] = 'used in farming'},
     ["corn"]         = {["name"] = "corn",         ["label"] = "Corn",          ["weight"] = 100, ["type"] = "item", ["image"] = "corn.png",        ['unique'] = false, ['useable'] = true, ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ["description"] = "product from farming"},
