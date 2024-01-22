109783c0b61f09d645daef919dd7ed595f2881b8
RexshackGaming <rexshackuk@gmail.com>
2024-01-22 18:00:59 +0000

    add blood money clip item

--- shared/items.lua
+++ shared/items.lua
@@ -159,6 +159,7 @@ RSGShared.Items = {
     ['birdpost'] = {['name'] = 'birdpost', ['label'] = 'Bird Post', ['weight'] = 5, ['type'] = 'item', ['image'] = 'birdpost.png', ['unique'] = false, ['useable'] = true, ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ['description'] = 'Bird Post for sending letters'},
 
     -- Money Clip
-    ['moneyclip'] = {['name'] = 'moneyclip', ['label'] = 'Money Clip', ['weight'] = 1, ['type'] = 'item', ['image'] = 'money_moneyclip.png', ['unique'] = true, ['useable'] = true, ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ['description'] = 'Money Clip'},
+    ['moneyclip']      = {['name'] = 'moneyclip',      ['label'] = 'Money Clip',       ['weight'] = 1, ['type'] = 'item', ['image'] = 'money_moneyclip.png',      ['unique'] = true, ['useable'] = true, ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ['description'] = 'Money Clip'},
+    ['bloodmoneyclip'] = {['name'] = 'bloodmoneyclip', ['label'] = 'Blood Money Clip', ['weight'] = 1, ['type'] = 'item', ['image'] = 'bloodmoney_moneyclip.png', ['unique'] = true, ['useable'] = true, ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ['description'] = 'Blood Money Clip'},
     
 }
