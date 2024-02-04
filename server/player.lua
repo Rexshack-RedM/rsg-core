aa764aa63e1810aa67192e91d4e610af026715da
RexshackGaming <rexshackuk@gmail.com>
2024-02-04 04:57:12 +0000

    remove playerclothe

--- server/player.lua
+++ server/player.lua
@@ -648,7 +648,6 @@ end
 
 local playertables = { -- Add tables as needed
     { table = "players"},
-    { table = "playerclothe"},
     { table = "playeroutfit"},
     { table = "playerskins"},
     { table = "player_horses"},
