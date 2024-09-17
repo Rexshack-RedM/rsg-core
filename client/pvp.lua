be2f89e611ab4532f0a43fb89430cf078ad48520
VegasJ <86978563+VegasJohnson@users.noreply.github.com>
2024-09-16 21:05:32 -0500

    Update pvp.lua
    fix pvp exploit

--- client/pvp.lua
+++ client/pvp.lua
@@ -1,6 +1,6 @@
 local pvp = true
 
-RegisterNetEvent('rsg-core:client:pvpToggle',function()
+-[[RegisterNetEvent('rsg-core:client:pvpToggle',function()
 
   if pvp == true then
         SetRelationshipBetweenGroups(1,`PLAYER`, `PLAYER`)
@@ -35,5 +35,40 @@ CreateThread(function()
         end
 
         Wait(5)
+    end
+end)]]
+CreateThread(function()
+    local active = false
+    local timer = 0
+    while true do 
+        Wait(0)
+        if active == false and not IsPedOnMount(PlayerPedId()) and not IsPedInAnyVehicle(PlayerPedId()) then
+            SetRelationshipBetweenGroups(3, `PLAYER`, `PLAYER`)
+            
+        else
+            SetRelationshipBetweenGroups(1, `PLAYER`, `PLAYER`)
+        end
+
+        if IsControlJustPressed(0,0xCEFD9220) then -- E KEY
+            timer = 0
+            active = true
+            while  timer < 200 do 
+                Wait(0)
+                timer = timer + 1
+                SetRelationshipBetweenGroups(1, `PLAYER`, `PLAYER`)
+            end
+            
+            active = false
+        end
+
+        if IsControlJustPressed(0,0xB2F377E8) then -- F KEY
+            Wait(500)
+            SetRelationshipBetweenGroups(1, `PLAYER`, `PLAYER`)
+            active = false
+            timer = 0
+        end
+                
+        Citizen.InvokeNative(0xF808475FA571D823, true) --enable friendly fire
+        NetworkSetFriendlyFireOption(true)
     end
 end)
