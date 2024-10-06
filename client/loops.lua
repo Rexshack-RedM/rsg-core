d2ba2ff62087de1a325b3865f6e64c4f6f72c6f2
RexshackGaming <rexshackuk@gmail.com>
2024-10-06 06:13:45 +0100

    core health loop udpate

--- client/loops.lua
+++ client/loops.lua
@@ -12,11 +12,14 @@ end)
 CreateThread(function()
     while true do
         if LocalPlayer.state.isLoggedIn then
-            if (RSGCore.PlayerData.metadata.hunger <= 0 or RSGCore.PlayerData.metadata.thirst <= 0) and not (RSGCore.PlayerData.metadata.isdead or RSGCore.PlayerData.metadata.inlaststand) then
-                local ped = PlayerPedId()
-                local currentHealth = GetEntityHealth(ped)
-                local decreaseThreshold = math.random(5, 10)
-                SetEntityHealth(ped, currentHealth - decreaseThreshold)
+            if (RSGCore.PlayerData.metadata['hunger'] <= 0 or RSGCore.PlayerData.metadata['thirst'] <= 0) and not RSGCore.PlayerData.metadata['isdead'] then
+                local currentHealth = GetEntityHealth(cache.ped)
+                if currentHealth < 10 then
+                    SetEntityHealth(cache.ped, 0)
+                else
+                    local decreaseThreshold = math.random(5, 10)
+                    SetEntityHealth(cache.ped, currentHealth - decreaseThreshold)
+                end
             end
         end
         Wait(RSGCore.Config.StatusInterval)
