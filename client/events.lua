69e88029b6b67961bd92b2df48545766d0a9f0b9
HardBaited <pantelisxanthos@gmail.com>
2024-10-19 00:43:14 +0300

    Add option for hidden player names

--- client/events.lua
+++ client/events.lua
@@ -1,213 +1,233 @@
 -- Place Ped on ground properly
 local function PlacePedOnGroundProperly(ped, coord)
-    local x, y, z = table.unpack(coord)
-    local found, groundz, normal = GetGroundZAndNormalFor_3dCoord(x, y, z)
+	local x, y, z = table.unpack(coord)
+	local found, groundz, normal = GetGroundZAndNormalFor_3dCoord(x, y, z)
 
-    if found then
-        SetEntityCoordsNoOffset(ped, x, y, groundz + normal.z, true)
-    end
+	if found then
+		SetEntityCoordsNoOffset(ped, x, y, groundz + normal.z, true)
+	end
 end
 
 -- Player load and unload handling
 -- New method for checking if logged in across all scripts (optional)
 -- if LocalPlayer.state['isLoggedIn'] then
-RegisterNetEvent('RSGCore:Client:OnPlayerLoaded', function()
-    ShutdownLoadingScreenNui()
-    LocalPlayer.state:set('isLoggedIn', true, false)
-    --if not RSGCore.Config.Server.PVP then return end
-    --SetCanAttackFriendly(PlayerPedId(), true, false)
-    --NetworkSetFriendlyFireOption(true)
-    if RSGConfig.EnablePVP then
-        Citizen.InvokeNative(0xF808475FA571D823, true)
-        SetRelationshipBetweenGroups(5, `PLAYER`, `PLAYER`)
-    end
-    if RSGConfig.Player.RevealMap then
-        SetMinimapHideFow(true)
-    end
-    Citizen.InvokeNative(0x39363DFD04E91496, PlayerId(), true) -- enable mercy kil
-    Citizen.InvokeNative(0x8899C244EBCF70DE, PlayerPedId(), 0.0) -- SetPlayerHealthRechargeMultiplier
-    Citizen.InvokeNative(0xDE1B1907A83A1550, PlayerPedId(), 0.0) -- SetHealthRechargeMultiplier
+RegisterNetEvent("RSGCore:Client:OnPlayerLoaded", function()
+	ShutdownLoadingScreenNui()
+	LocalPlayer.state:set("isLoggedIn", true, false)
+	--if not RSGCore.Config.Server.PVP then return end
+	--SetCanAttackFriendly(PlayerPedId(), true, false)
+	--NetworkSetFriendlyFireOption(true)
+	if RSGConfig.EnablePVP then
+		Citizen.InvokeNative(0xF808475FA571D823, true)
+		SetRelationshipBetweenGroups(5, `PLAYER`, `PLAYER`)
+	end
+	if RSGConfig.Player.RevealMap then
+		SetMinimapHideFow(true)
+	end
+	Citizen.InvokeNative(0x39363DFD04E91496, PlayerId(), true) -- enable mercy kil
+	Citizen.InvokeNative(0x8899C244EBCF70DE, PlayerPedId(), 0.0) -- SetPlayerHealthRechargeMultiplier
+	Citizen.InvokeNative(0xDE1B1907A83A1550, PlayerPedId(), 0.0) -- SetHealthRechargeMultiplier
 end)
 
-RegisterNetEvent('RSGCore:Client:OnPlayerUnload', function()
-    LocalPlayer.state:set('isLoggedIn', false, false)
+RegisterNetEvent("RSGCore:Client:OnPlayerUnload", function()
+	LocalPlayer.state:set("isLoggedIn", false, false)
 end)
 
-RegisterNetEvent('RSGCore:Client:PvpHasToggled', function(pvp_state)
-    SetCanAttackFriendly(PlayerPedId(), pvp_state, false)
-    NetworkSetFriendlyFireOption(pvp_state)
+RegisterNetEvent("RSGCore:Client:PvpHasToggled", function(pvp_state)
+	SetCanAttackFriendly(PlayerPedId(), pvp_state, false)
+	NetworkSetFriendlyFireOption(pvp_state)
 end)
 
 -- Teleport Commands
 
-RegisterNetEvent('RSGCore:Command:TeleportToPlayer', function(coords)
-    SetEntityCoords(cache.ped, coords.x, coords.y, coords.z) 
+RegisterNetEvent("RSGCore:Command:TeleportToPlayer", function(coords)
+	SetEntityCoords(cache.ped, coords.x, coords.y, coords.z)
 end)
 
-RegisterNetEvent('RSGCore:Command:TeleportToCoords', function(x, y, z, h)
-    SetEntityCoords(cache.ped, x, y, z) 
+RegisterNetEvent("RSGCore:Command:TeleportToCoords", function(x, y, z, h)
+	SetEntityCoords(cache.ped, x, y, z)
 end)
 
-RegisterNetEvent('RSGCore:Command:GoToMarker', function()
-    local coords = GetWaypointCoords()
-    local groundZ = GetHeightmapBottomZForPosition(coords.x, coords.y)
-    local vehicle = GetVehiclePedIsIn(cache.ped, false)
-    if not IsWaypointActive() then
-        lib.notify({ title = Lang:t("error.no_waypoint"), type = 'error', duration = 5000 })
-        return
-    end
+RegisterNetEvent("RSGCore:Command:GoToMarker", function()
+	local coords = GetWaypointCoords()
+	local groundZ = GetHeightmapBottomZForPosition(coords.x, coords.y)
+	local vehicle = GetVehiclePedIsIn(cache.ped, false)
+	if not IsWaypointActive() then
+		lib.notify({ title = Lang:t("error.no_waypoint"), type = "error", duration = 5000 })
+		return
+	end
 
-    SetEntityCoords(cache.ped, coords.x, coords.y, groundZ + 3.0)
-    PlacePedOnGroundProperly(cache.ped, coords)
+	SetEntityCoords(cache.ped, coords.x, coords.y, groundZ + 3.0)
+	PlacePedOnGroundProperly(cache.ped, coords)
 
-    if cache.mount then
-        SetEntityCoords(cache.mount, coords.x, coords.y, groundZ + 3.0)
-        PlacePedOnGroundProperly(cache.mount, coords)
-        Citizen.InvokeNative(0x028F76B6E78246EB, cache.ped, cache.mount, -1)
-    end
+	if cache.mount then
+		SetEntityCoords(cache.mount, coords.x, coords.y, groundZ + 3.0)
+		PlacePedOnGroundProperly(cache.mount, coords)
+		Citizen.InvokeNative(0x028F76B6E78246EB, cache.ped, cache.mount, -1)
+	end
 
-    if vehicle then
-        SetEntityCoords(vehicle, coords.x, coords.y, groundZ + 3.0)
-        PlacePedOnGroundProperly(vehicle, coords)
-        Citizen.InvokeNative(0x028F76B6E78246EB, cache.ped, vehicle, -1)
-    end
+	if vehicle then
+		SetEntityCoords(vehicle, coords.x, coords.y, groundZ + 3.0)
+		PlacePedOnGroundProperly(vehicle, coords)
+		Citizen.InvokeNative(0x028F76B6E78246EB, cache.ped, vehicle, -1)
+	end
 
-    lib.notify({ title = Lang:t("success.teleported_waypoint"), type = 'success', duration = 5000 })
+	lib.notify({ title = Lang:t("success.teleported_waypoint"), type = "success", duration = 5000 })
 end)
 
 -- Noclip Command
-RegisterNetEvent('RSGCore:Command:ToggleNoClip', function()
-    ExecuteCommand('txAdmin:menu:noClipToggle')
+RegisterNetEvent("RSGCore:Command:ToggleNoClip", function()
+	ExecuteCommand("txAdmin:menu:noClipToggle")
 end)
 
 -- Vehicle Commands
 
-RegisterNetEvent('RSGCore:Command:SpawnVehicle', function(vehName)
-    local ped = PlayerPedId()
-    local hash = joaat(vehName)
-    local veh = GetVehiclePedIsUsing(ped)
-    if not IsModelInCdimage(hash) then return end
-    RequestModel(hash)
-    while not HasModelLoaded(hash) do
-        Wait(0)
-    end
+RegisterNetEvent("RSGCore:Command:SpawnVehicle", function(vehName)
+	local ped = PlayerPedId()
+	local hash = joaat(vehName)
+	local veh = GetVehiclePedIsUsing(ped)
+	if not IsModelInCdimage(hash) then
+		return
+	end
+	RequestModel(hash)
+	while not HasModelLoaded(hash) do
+		Wait(0)
+	end
 
-    if IsPedInAnyVehicle(ped) then
-        SetEntityAsMissionEntity(veh, true, true)
-        DeleteVehicle(veh)
-    end
+	if IsPedInAnyVehicle(ped) then
+		SetEntityAsMissionEntity(veh, true, true)
+		DeleteVehicle(veh)
+	end
 
-    local vehicle = CreateVehicle(hash, GetEntityCoords(ped), GetEntityHeading(ped), true, false)
-    TaskWarpPedIntoVehicle(ped, vehicle, -1)
-    SetVehicleFuelLevel(vehicle, 100.0)
-    SetVehicleDirtLevel(vehicle, 0.0)
-    SetModelAsNoLongerNeeded(hash)
+	local vehicle = CreateVehicle(hash, GetEntityCoords(ped), GetEntityHeading(ped), true, false)
+	TaskWarpPedIntoVehicle(ped, vehicle, -1)
+	SetVehicleFuelLevel(vehicle, 100.0)
+	SetVehicleDirtLevel(vehicle, 0.0)
+	SetModelAsNoLongerNeeded(hash)
 end)
 
-RegisterNetEvent('RSGCore:Command:DeleteVehicle', function()
-    local ped = PlayerPedId()
-    local veh = GetVehiclePedIsUsing(ped)
-    if veh ~= 0 then
-        SetEntityAsMissionEntity(veh, true, true)
-        DeleteVehicle(veh)
-    else
-        local pcoords = GetEntityCoords(ped)
-        local vehicles = GetGamePool('CVehicle')
-        for _, v in pairs(vehicles) do
-            if #(pcoords - GetEntityCoords(v)) <= 5.0 then
-                SetEntityAsMissionEntity(v, true, true)
-                DeleteVehicle(v)
-            end
-        end
-    end
+RegisterNetEvent("RSGCore:Command:DeleteVehicle", function()
+	local ped = PlayerPedId()
+	local veh = GetVehiclePedIsUsing(ped)
+	if veh ~= 0 then
+		SetEntityAsMissionEntity(veh, true, true)
+		DeleteVehicle(veh)
+	else
+		local pcoords = GetEntityCoords(ped)
+		local vehicles = GetGamePool("CVehicle")
+		for _, v in pairs(vehicles) do
+			if #(pcoords - GetEntityCoords(v)) <= 5.0 then
+				SetEntityAsMissionEntity(v, true, true)
+				DeleteVehicle(v)
+			end
+		end
+	end
 end)
 
 -- Other stuff
 
-RegisterNetEvent('RSGCore:Player:SetPlayerData', function(val)
-    RSGCore.PlayerData = val
+RegisterNetEvent("RSGCore:Player:SetPlayerData", function(val)
+	RSGCore.PlayerData = val
 end)
 
-RegisterNetEvent('RSGCore:Player:UpdatePlayerData', function()
-    TriggerServerEvent('RSGCore:UpdatePlayer')
+RegisterNetEvent("RSGCore:Player:UpdatePlayerData", function()
+	TriggerServerEvent("RSGCore:UpdatePlayer")
 end)
 
 -- This event is exploitable and should not be used. It has been deprecated, and will be removed soon.
-RegisterNetEvent('RSGCore:Client:UseItem', function(item)
-    RSGCore.Debug(string.format('%s triggered RSGCore:Client:UseItem by ID %s with the following data. This event is deprecated due to exploitation, and will be removed soon. Check qb-inventory for the right use on this event.', GetInvokingResource(), GetPlayerServerId(PlayerId())))
-    RSGCore.Debug(item)
+RegisterNetEvent("RSGCore:Client:UseItem", function(item)
+	RSGCore.Debug(
+		string.format(
+			"%s triggered RSGCore:Client:UseItem by ID %s with the following data. This event is deprecated due to exploitation, and will be removed soon. Check qb-inventory for the right use on this event.",
+			GetInvokingResource(),
+			GetPlayerServerId(PlayerId())
+		)
+	)
+	RSGCore.Debug(item)
 end)
 
 -- Callback Events --
 
 -- Client Callback
-RegisterNetEvent('RSGCore:Client:TriggerClientCallback', function(name, ...)
-    RSGCore.Functions.TriggerClientCallback(name, function(...)
-        TriggerServerEvent('RSGCore:Server:TriggerClientCallback', name, ...)
-    end, ...)
+RegisterNetEvent("RSGCore:Client:TriggerClientCallback", function(name, ...)
+	RSGCore.Functions.TriggerClientCallback(name, function(...)
+		TriggerServerEvent("RSGCore:Server:TriggerClientCallback", name, ...)
+	end, ...)
 end)
 
 -- Server Callback
-RegisterNetEvent('RSGCore:Client:TriggerCallback', function(name, ...)
-    if RSGCore.ServerCallbacks[name] then
-        RSGCore.ServerCallbacks[name](...)
-        RSGCore.ServerCallbacks[name] = nil
-    end
+RegisterNetEvent("RSGCore:Client:TriggerCallback", function(name, ...)
+	if RSGCore.ServerCallbacks[name] then
+		RSGCore.ServerCallbacks[name](...)
+		RSGCore.ServerCallbacks[name] = nil
+	end
 end)
 
 -- Me command
 local function Draw3DText(coords, str)
-    local onScreen, worldX, worldY = GetScreenCoordFromWorldCoord(coords.x, coords.y, coords.z)
-    local camCoords = GetGameplayCamCoord()
-    local scale = 200 / (GetGameplayCamFov() * #(camCoords - coords))
+	local onScreen, worldX, worldY = GetScreenCoordFromWorldCoord(coords.x, coords.y, coords.z)
+	local camCoords = GetGameplayCamCoord()
+	local scale = 200 / (GetGameplayCamFov() * #(camCoords - coords))
 
-    if onScreen then
-        -- Set the text color using SetTextColor (RedM version)
-        SetTextColor(255, 255, 255, 255) -- White color with full opacity
+	if onScreen then
+		-- Set the text color using SetTextColor (RedM version)
+		SetTextColor(255, 255, 255, 255) -- White color with full opacity
 
-        -- Set the text scale (RedM requires slight adjustment)
-        SetTextScale(0.0, 0.5 * scale) -- Adjust the scale values as needed
+		-- Set the text scale (RedM requires slight adjustment)
+		SetTextScale(0.0, 0.5 * scale) -- Adjust the scale values as needed
 
-        -- Set the font to the desired font using SetTextFontForCurrentCommand
-        SetTextFontForCurrentCommand(2) -- Use appropriate font ID here
+		-- Set the font to the desired font using SetTextFontForCurrentCommand
+		SetTextFontForCurrentCommand(2) -- Use appropriate font ID here
 
-        -- Center the text
-        SetTextCentre(true)
+		-- Center the text
+		SetTextCentre(true)
 
-        -- Create the text to be displayed using a variable string
-        local varString = CreateVarString(10, "LITERAL_STRING", str)
+		-- Create the text to be displayed using a variable string
+		local varString = CreateVarString(10, "LITERAL_STRING", str)
 
-        -- Display the text at the world coordinates (converted to screen coordinates)
-        DisplayText(varString, worldX, worldY)
-    end
+		-- Display the text at the world coordinates (converted to screen coordinates)
+		DisplayText(varString, worldX, worldY)
+	end
 end
 
-RegisterNetEvent('RSGCore:Command:ShowMe3D', function(senderId, msg)
-    local sender = GetPlayerFromServerId(senderId)
-    CreateThread(function()
-        local displayTime = 10000 + GetGameTimer()
-        while displayTime > GetGameTimer() do
-            local targetPed = GetPlayerPed(sender)
-            local tCoords = GetEntityCoords(targetPed)
-            Draw3DText(tCoords, msg)
-            Wait(0)
-        end
-    end)
+RegisterNetEvent("RSGCore:Command:ShowMe3D", function(senderId, msg)
+	local sender = GetPlayerFromServerId(senderId)
+	CreateThread(function()
+		local displayTime = 10000 + GetGameTimer()
+		while displayTime > GetGameTimer() do
+			local targetPed = GetPlayerPed(sender)
+			local tCoords = GetEntityCoords(targetPed)
+			Draw3DText(tCoords, msg)
+			Wait(0)
+		end
+	end)
 end)
 
 -- Listen to Shared being updated
-RegisterNetEvent('RSGCore:Client:OnSharedUpdate', function(tableName, key, value)
-    RSGCore.Shared[tableName][key] = value
-    TriggerEvent('RSGCore:Client:UpdateObject')
+RegisterNetEvent("RSGCore:Client:OnSharedUpdate", function(tableName, key, value)
+	RSGCore.Shared[tableName][key] = value
+	TriggerEvent("RSGCore:Client:UpdateObject")
 end)
 
-RegisterNetEvent('RSGCore:Client:OnSharedUpdateMultiple', function(tableName, values)
-    for key, value in pairs(values) do
-        RSGCore.Shared[tableName][key] = value
-    end
-    TriggerEvent('RSGCore:Client:UpdateObject')
+RegisterNetEvent("RSGCore:Client:OnSharedUpdateMultiple", function(tableName, values)
+	for key, value in pairs(values) do
+		RSGCore.Shared[tableName][key] = value
+	end
+	TriggerEvent("RSGCore:Client:UpdateObject")
 end)
 
-RegisterNetEvent('RSGCore:Client:SharedUpdate', function(table)
-    RSGCore.Shared = table
+RegisterNetEvent("RSGCore:Client:SharedUpdate", function(table)
+	RSGCore.Shared = table
 end)
+
+if RSGConfig.HidePlayerNames then
+	CreateThread(function()
+		while true do
+			Wait(5000)
+			for _, player in ipairs(GetActivePlayers()) do
+				local ped = GetPlayerPed(player)
+				SetPedPromptName(ped, "Stranger (" .. tostring(GetPlayerServerId(player)) .. ")")
+			end
+		end
+	end)
+end
--- config.lua
+++ config.lua
@@ -1,103 +1,121 @@
 RSGConfig = {}
 
-RSGConfig.MaxPlayers = GetConvarInt('sv_maxclients', 48) -- Gets max players from config file, default 48
+RSGConfig.MaxPlayers = GetConvarInt("sv_maxclients", 48) -- Gets max players from config file, default 48
 RSGConfig.DefaultSpawn = vector4(-1035.71, -2731.87, 12.86, 0.0)
-RSGConfig.UpdateInterval = 5                             -- how often to update player data in minutes
-RSGConfig.StatusInterval = 5000                          -- how often to check hunger/thirst status in milliseconds
+RSGConfig.UpdateInterval = 5 -- how often to update player data in minutes
+RSGConfig.StatusInterval = 5000 -- how often to check hunger/thirst status in milliseconds
+RSGConfig.HidePlayerNames = true
 
 RSGConfig.Money = {}
 RSGConfig.Money.MoneyTypes = { cash = 50, bank = 0, valbank = 0, rhobank = 0, blkbank = 0, armbank = 0, bloodmoney = 0 } -- type = startamount - Add or remove money types for your server (for ex. blackmoney = 0), remember once added it will not be removed from the database!
-RSGConfig.Money.DontAllowMinus = { 'cash', 'bloodmoney' } -- Money that is not allowed going in minus
-RSGConfig.Money.PayCheckTimeOut = 10                                 -- The time in minutes that it will give the paycheck
-RSGConfig.Money.PayCheckSociety = false                              -- If true paycheck will come from the society account that the player is employed at, requires rsg-management
+RSGConfig.Money.DontAllowMinus = { "cash", "bloodmoney" } -- Money that is not allowed going in minus
+RSGConfig.Money.PayCheckTimeOut = 10 -- The time in minutes that it will give the paycheck
+RSGConfig.Money.PayCheckSociety = false -- If true paycheck will come from the society account that the player is employed at, requires rsg-management
 
 RSGConfig.Player = {}
 RSGConfig.Player.HungerRate = 4.2 -- Rate at which hunger goes down.
 RSGConfig.Player.ThirstRate = 3.8 -- Rate at which thirst goes down.
 RSGConfig.Player.Bloodtypes = {
-    'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-',
+	"A+",
+	"A-",
+	"B+",
+	"B-",
+	"AB+",
+	"AB-",
+	"O+",
+	"O-",
 }
 
 RSGConfig.Player.PlayerDefaults = {
-    citizenid = function() return RSGCore.Player.CreateCitizenId() end,
-    cid = 1,
-    money = function()
-        local moneyDefaults = {}
-        for moneytype, startamount in pairs(RSGConfig.Money.MoneyTypes) do
-            moneyDefaults[moneytype] = startamount
-        end
-        return moneyDefaults
-    end,
-    optin = true,
-    charinfo = {
-        firstname = 'Firstname',
-        lastname = 'Lastname',
-        birthdate = '00-00-0000',
-        gender = 0,
-        nationality = 'USA',
-        account = function() return RSGCore.Functions.CreateAccountNumber() end
-    },
-    job = {
-        name = 'unemployed',
-        label = 'Civilian',
-        payment = 10,
-        type = 'none',
-        onduty = false,
-        isboss = false,
-        grade = {
-            name = 'Freelancer',
-            level = 0
-        }
-    },
-    gang = {
-        name = 'none',
-        label = 'No Gang Affiliation',
-        isboss = false,
-        grade = {
-            name = 'none',
-            level = 0
-        }
-    },
-    metadata = {
-        health = 600,
-        hunger = 100,
-        thirst = 100,
-        cleanliness = 100,
-        stress = 0,
-        isdead = false,
-        armor = 0,
-        ishandcuffed = false,
-        injail = 0,
-        jailitems = {},
-        status = {},
-        rep = {},
-        callsign = 'NO CALLSIGN',
-        bloodtype = function() return RSGConfig.Player.Bloodtypes[math.random(1, #RSGConfig.Player.Bloodtypes)] end,
-        fingerprint = function() return RSGCore.Player.CreateFingerId() end,
-        walletid = function() return RSGCore.Player.CreateWalletId() end,
-        criminalrecord = {
-            hasRecord = false,
-            date = nil
-        },
-    },
-    position = RSGConfig.DefaultSpawn,
-    items = {},
+	citizenid = function()
+		return RSGCore.Player.CreateCitizenId()
+	end,
+	cid = 1,
+	money = function()
+		local moneyDefaults = {}
+		for moneytype, startamount in pairs(RSGConfig.Money.MoneyTypes) do
+			moneyDefaults[moneytype] = startamount
+		end
+		return moneyDefaults
+	end,
+	optin = true,
+	charinfo = {
+		firstname = "Firstname",
+		lastname = "Lastname",
+		birthdate = "00-00-0000",
+		gender = 0,
+		nationality = "USA",
+		account = function()
+			return RSGCore.Functions.CreateAccountNumber()
+		end,
+	},
+	job = {
+		name = "unemployed",
+		label = "Civilian",
+		payment = 10,
+		type = "none",
+		onduty = false,
+		isboss = false,
+		grade = {
+			name = "Freelancer",
+			level = 0,
+		},
+	},
+	gang = {
+		name = "none",
+		label = "No Gang Affiliation",
+		isboss = false,
+		grade = {
+			name = "none",
+			level = 0,
+		},
+	},
+	metadata = {
+		health = 600,
+		hunger = 100,
+		thirst = 100,
+		cleanliness = 100,
+		stress = 0,
+		isdead = false,
+		armor = 0,
+		ishandcuffed = false,
+		injail = 0,
+		jailitems = {},
+		status = {},
+		rep = {},
+		callsign = "NO CALLSIGN",
+		bloodtype = function()
+			return RSGConfig.Player.Bloodtypes[math.random(1, #RSGConfig.Player.Bloodtypes)]
+		end,
+		fingerprint = function()
+			return RSGCore.Player.CreateFingerId()
+		end,
+		walletid = function()
+			return RSGCore.Player.CreateWalletId()
+		end,
+		criminalrecord = {
+			hasRecord = false,
+			date = nil,
+		},
+	},
+	position = RSGConfig.DefaultSpawn,
+	items = {},
 }
 
-RSGConfig.Server = {}                                    -- General server config
-RSGConfig.Server.Closed = false                          -- Set server closed (no one can join except people with ace permission 'rsgadmin.join')
-RSGConfig.Server.ClosedReason = 'Server Closed'          -- Reason message to display when people can't join the server
-RSGConfig.Server.Uptime = 0                              -- Time the server has been up.
-RSGConfig.Server.Whitelist = false                       -- Enable or disable whitelist on the server
-RSGConfig.Server.WhitelistPermission = 'admin'           -- Permission that's able to enter the server when the whitelist is on
-RSGConfig.Server.PVP = true                              -- Enable or disable pvp on the server (Ability to shoot other players)
+RSGConfig.Server = {} -- General server config
+RSGConfig.Server.Closed = false -- Set server closed (no one can join except people with ace permission 'rsgadmin.join')
+RSGConfig.Server.ClosedReason = "Server Closed" -- Reason message to display when people can't join the server
+RSGConfig.Server.Uptime = 0 -- Time the server has been up.
+RSGConfig.Server.Whitelist = false -- Enable or disable whitelist on the server
+RSGConfig.Server.WhitelistPermission = "admin" -- Permission that's able to enter the server when the whitelist is on
+RSGConfig.Server.PVP = true -- Enable or disable pvp on the server (Ability to shoot other players)
 RSGConfig.EnablePVP = true
-RSGConfig.Server.Discord = ''                            -- Discord invite link
-RSGConfig.Server.CheckDuplicateLicense = true            -- Check for duplicate rockstar license on join
-RSGConfig.Server.Permissions = { 'god', 'admin', 'mod' } -- Add as many groups as you want here after creating them in your server.cfg
+RSGConfig.Server.Discord = "" -- Discord invite link
+RSGConfig.Server.CheckDuplicateLicense = true -- Check for duplicate rockstar license on join
+RSGConfig.Server.Permissions = { "god", "admin", "mod" } -- Add as many groups as you want here after creating them in your server.cfg
 
-RSGConfig.Commands = {}                                  -- Command Configuration
-RSGConfig.Commands.OOCColor = { 255, 151, 133 }          -- RGB color code for the OOC command
+RSGConfig.Commands = {} -- Command Configuration
+RSGConfig.Commands.OOCColor = { 255, 151, 133 } -- RGB color code for the OOC command
 
 RSGConfig.PromptDistance = 1.5
 RSGConfig.Player.CleanlinessRate = 0.0 -- Rate at which cleanliness goes down.
