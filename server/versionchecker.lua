f4ba51a8389384835efc43b8f265fc80a1fa46ae
RexShack <76662933+RexShack@users.noreply.github.com>
2025-03-06 06:10:54 +0000

    Update versionchecker.lua

--- server/versionchecker.lua
+++ server/versionchecker.lua
@@ -10,7 +10,7 @@ local function versionCheckPrint(_type, log)
 end
 
 local function CheckVersion()
-    PerformHttpRequest('https://raw.githubusercontent.com/RexShack/rsg-versioncheckers/main/'..GetCurrentResourceName()..'/version.txt', function(err, text, headers)
+    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-versioncheckers/main/'..GetCurrentResourceName()..'/version.txt', function(err, text, headers)
         local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
 
         if not text then
